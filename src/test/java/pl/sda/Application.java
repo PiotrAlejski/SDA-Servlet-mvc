fd8f0f36f65f5dd0e7358032daee58f5b63a7438
unknown <piotr.alejski@gmail.com>
2017-03-01 19:20:31 +0100

    Initial commit

---
+++ src/test/java/pl/sda/Application.java
@@ -0,0 +1,10 @@
+package pl.sda;
+
+/**
+ * Created by RENT on 2017-03-01.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
