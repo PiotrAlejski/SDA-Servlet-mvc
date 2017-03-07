6f7112776b1ed662e9c1460ccebd02e4b2808717
unknown <piotr.alejski@gmail.com>
2017-03-07 21:02:35 +0100

    update Servlet

--- src/main/java/pl/sda/servlet/HelloServlet.java
+++ src/main/java/pl/sda/servlet/HelloServlet.java
@@ -1,16 +1,21 @@
 package pl.sda.servlet;
 
 import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
 import pl.sda.controller.Controller;
 import pl.sda.controller.CookieController;
 import pl.sda.controller.LoginController;
 import pl.sda.controller.UserController;
+import pl.sda.file.FileOperations;
+import pl.sda.file.User;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -44,5 +49,23 @@ public class HelloServlet extends HttpServlet {
             controller = controllerMap.get("default");
         }
         controller.handleGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
+        String body = reader.readLine();
+        String[] split = body.split(" ");
+
+        User user = new User();
+        user.setFirstName(split[0]);
+        user.setLastName(split[1]);
+        user.setAge(new Integer(split[2]));
+
+        File file = new File("C:\\Users\\RENT\\Desktop\\test.txt");
+        FileOperations.addUserToFile(user, file);
+
+        resp.getWriter().write("Wszystko ok");
+        resp.setStatus(201);
     }
 }
